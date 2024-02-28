5b5f8050be52d5e5c485389ae1e022621617fda0
aizadramli90 <muhammadaizadr@gmail.com>
2024-02-28 20:24:11 +0800

    Wordle solver completed

---
+++ wordle.js
@@ -0,0 +1,35 @@
+
+// get list of all 5 letter words
+const allWords = require('./words');
+// has these letters
+const hasLetters = ['e', 'd']; 
+// must not has these letters
+const mustNotHaveLetters = ['a', 'r', 'y', 'm', 's'];
+// must have selected letters in these positions
+const mustHaveLettersInPosition = [,,'d','e',]
+// not in these positions
+const mustNotHaveLettersInPosition = [,'e','e','d','d']
+
+const containsLetter = (word, letter) => {
+  return word.includes(letter);
+}
+
+/* const doesNotContainLetter = (word, letter) => {
+  return !word.includes(letter);
+} */
+
+const hasLetterInPosition = (word, letter, position) => {
+  if (letter === undefined) return true;
+
+  return word[position] === letter;
+}
+
+const filteredWords = allWords.filter(word => {
+  return hasLetters.every(letter => containsLetter(word, letter)) &&
+  mustNotHaveLetters.every(letter => !containsLetter(word, letter)) &&
+  mustHaveLettersInPosition.every((letter, index) => hasLetterInPosition(word, letter, index)) &&
+  mustNotHaveLettersInPosition.every((letter, index) => !hasLetterInPosition(word, letter, index))
+})
+
+console.log(filteredWords);
+console.log('possible words', filteredWords.length);
